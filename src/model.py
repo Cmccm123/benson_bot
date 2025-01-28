1b34d72ff164ea1292720aaf5aa9035e35de68f6
super_pc <super_pc>
2025-01-28 21:44:20 +0800

    update

--- src/model.py
+++ src/model.py
@@ -6,20 +6,19 @@ from threading import Thread
 from operator import itemgetter
 device = "cuda" # the device to load the model onto
 chroma_client = chromadb.PersistentClient(path="benson")
-model_path = "deepseek-ai/DeepSeek-R1-Distill-Llama-70B"
-quant_config_4bit = BitsAndBytesConfig(
-    load_in_4bit=True,                  
-    bnb_4bit_compute_dtype=torch.float16, 
-    bnb_4bit_use_double_quant=True,     
-    bnb_4bit_quant_type="nf4",        
-)
+model_path = "deepseek-ai/DeepSeek-R1-Distill-Qwen-14B"
+# quant_config_4bit = BitsAndBytesConfig(
+#     load_in_4bit=True,                  
+#     bnb_4bit_compute_dtype=torch.float16, 
+#     bnb_4bit_use_double_quant=True,     
+#     bnb_4bit_quant_type="nf4",        
+# )
 
 
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 
 model = AutoModelForCausalLM.from_pretrained(
     model_path,
-    quantization_config=quant_config_4bit,
     torch_dtype="auto",
     device_map="auto"
 )
@@ -32,7 +31,7 @@ reranker = AutoModelForSequenceClassification.from_pretrained(
 
 emb_model = AutoModel.from_pretrained('jinaai/jina-embeddings-v2-base-zh', trust_remote_code=True)
 
-def generate_response(prompt,history=[],tpt=0.45):
+def generate_response(inp,history=[],tpt=0.35):
     key_word_messages = [
         {"role": "system", "content": "你是關鍵字搜尋器,你需要為一句句子創造關鍵字,關鍵字並不一定是句子中的詞語,而是要最能夠幫助系統在數據庫中搜尋到有用資料的關鍵字,\n你只能輸出搜尋搜尋名詞，也就是最終結果，不能輸出其他東西"},
         {"role": "user", "content": f"你好，Google"},
@@ -43,7 +42,7 @@ def generate_response(prompt,history=[],tpt=0.45):
         {"role": "assistant", "content": "宇宙大爆炸"},
         {"role": "user", "content": f"'對於香港it的生態環境有什麼看法"},
         {"role": "assistant", "content": "香港it的看法"},
-        {"role": "user", "content": f"{prompt}"}
+        {"role": "user", "content": f"{inp}"}
     ]
     
     text = tokenizer.apply_chat_template(
@@ -60,9 +59,9 @@ def generate_response(prompt,history=[],tpt=0.45):
     generated_ids = [
         output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
     ]
-
+    
     key_word_response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
-    print(key_word_response)
+    print(key_word_response.split("</think>")[1])
     embeddings_result =[]
     embeddings = emb_model.encode([str(key_word_response)])
     collection = chroma_client.get_collection(name="benson")
@@ -103,7 +102,7 @@ def generate_response(prompt,history=[],tpt=0.45):
         
     embeddings_final_result.sort(reverse = True,key=itemgetter('score'))
     
-    inp = f"""
+    prompt = f"""
     你是Benson，
     你是青衣ive 科目編號:IT114116 數據科學及分析科(簡稱DSA)老師
     你對待學生不需要非常有禮貌,面對住學生的質疑,你會直接反駁,如果學生對你說一些負面說話,你會直接斥責
@@ -111,22 +110,23 @@ def generate_response(prompt,history=[],tpt=0.45):
     
     以下是Benson語錄,當中的資訊能夠協助你回答問題
     
-    Benson語錄(請你選擇對於回答問題有用的語錄):"""
+    Benson語錄(請你選擇對於回答問題有用的語錄):\n"""
     
     
     for i in embeddings_final_result:
         if i["score"] > 0:
             print(i['context'])
             print("======")
-            inp += f"<context>{i['context']}</centext>\n"
+            if i['context'] != "":
+                prompt += f"<context>{i['context']}</centext>\n"
     
-    inp += "請使用廣東話"
-    print(inp)
+    prompt += "請使用廣東話"
+    print(prompt)
     messages = [
-        {"role": "system", "content": inp},
+        {"role": "system", "content": prompt},
     ]
     messages += history
-    messages.append({"role": "user", "content": prompt})
+    messages.append({"role": "user", "content": inp})
 
     text = tokenizer.apply_chat_template(
         messages,
